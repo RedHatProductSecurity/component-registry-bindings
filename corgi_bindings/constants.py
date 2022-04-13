54acf930e680a990b544d8da297a089f18fa5352
Jakub Frejlach <jfrejlac@redhat.com>
2022-04-13 17:38:38 +0200

    Implement session wrapper

---
+++ corgi_bindings/constants.py
@@ -0,0 +1,22 @@
+from typing import Dict, List
+
+CORGI_API_VERSION: str = "v1"
+CORGI_BINDINGS_USERAGENT: str = "corgi-bindings-0.0.1"
+CORGI_BINDINGS_API_PATH: str = f".bindings.python_client.api.{CORGI_API_VERSION}"
+
+# all available session operations
+ALL_SESSION_OPERATIONS: List[str] = (
+    "retrieve",
+    "update",
+    "list",
+    "create",
+    "destroy",
+    "search",
+)
+
+# mapping for resources which model names are different from the endpoint name
+RESOURCE_TO_MODEL_MAPPING: Dict[str, str] = {
+    "lifecycles": "AppStreamLifeCycle",
+    "components": "ComponentDetail",
+    "builds": "SoftwareBuilds",
+}
