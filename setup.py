74886457034470eaf6ef778ea2bdafaa87c01855
Jakub Frejlach <jfrejlac@redhat.com>
2022-04-20 14:34:54 +0200

    Setup package for installation

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="corgi-bindings",
+    version="0.0.1",
+    author="Jakub Frejlach, Red Hat Product Security",
+    author_email="jfrejlac@redhat.com",
+    description="Python bindings for accessing Component Registry (Corgi) API",
+    # url="https://github.com/RedHatProductSecurity/corgi-bindings", TODO: no github repo
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=["attrs", "httpx", "python-dateutil"],
+)
